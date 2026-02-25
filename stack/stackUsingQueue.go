3a02c4ebefeec40db88707e7eb9e3c07f29aa223
Aashish <aashishdubey.dev@gmail.com>
2026-02-26 05:07:36 +0530

    testing commit

---
+++ stack/stackUsingQueue.go
@@ -0,0 +1,24 @@
+package stack
+
+type MyStack struct {
+}
+
+func Constructor() MyStack {
+
+}
+
+func (this *MyStack) Push(x int) {
+
+}
+
+func (this *MyStack) Pop() int {
+
+}
+
+func (this *MyStack) Top() int {
+
+}
+
+func (this *MyStack) Empty() bool {
+
+}
